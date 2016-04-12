411e43a81310cfd2813042ccd8a987ef2b70410f
Ashley Smith <ashley22@vt.edu>
2016-04-11 20:27:41 -0700

    updates styling

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('sass', function(end) {
   gulp.src('./sass/style.scss')
   .pipe(sass())
   .on('error', sass.logError)
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+  }))
+  .pipe(cleanCSS())
   .pipe(gulp.dest('./'))
   .on('end', end)
+
 });
 
 gulp.task('default', ['sass', 'watch']) 
