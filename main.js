97baa353c7fa086fd059fe41782b947579d572c7
Ashley Smith <ashley22@vt.edu>
2016-04-29 00:27:27 -0700

    update slide in left animation

--- main.js
+++ main.js
@@ -52,6 +52,36 @@ function init() {
 })
 
 
+ var $animated = $('.animation');
+ var $window = $(window);
+ 
+ 
+ function check_if_in_view() {
+   var window_height = $window.height();
+   var window_top_position = $window.scrollTop();
+   var window_bottom_position = (window_top_position + window_height);
+
+   $.each($animated, function() {
+     var $element = $(this);
+     var element_height = $element.outerHeight();
+     var element_top_position = $element.offset().top;
+     var element_bottom_position = (element_top_position + element_height);
+
+
+     var animation = $element.data('animation')
+     if ((element_bottom_position >= window_top_position) &&
+       (element_top_position <= window_bottom_position)) {
+       $element.addClass('animated ' + animation);
+   } else {
+     $element.removeClass('animated ' + animation);
+
+   }
+ });
+ }
+
+ $window.on('scroll resize', check_if_in_view);
+ $window.trigger('scroll');
+
  $(window).scroll( function(){
   $('.box').each( function(i){
     var bottom_of_object = $(this).offset().top + $(this).outerHeight();
